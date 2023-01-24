7bd39c8581821845679f27dcf8da06595185b94a
orucmu <mustafaoruc720@gmail.com>
2023-01-24 16:05:16 +0300

    Socket Server'a Bağlanmak

---
+++ src/components/Palette.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Palette() {
+    return (
+        <div className='palette'>
+            <input type="color" />
+            <button>Click</button>
+        </div>
+
+    )
+}
+
+export default Palette
